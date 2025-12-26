6c4447bf71042f896b2bed12140a771b8b5147b6
phuongnamvu1004 <phuongnamvu.1004@gmail.com>
2025-12-25 20:01:11 -0500

    feat: developed Ordering.Application Queries

---
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/OrderCreatedEventHandler.cs
@@ -0,0 +1,11 @@
+namespace Ordering.Application.Orders.EventHandlers;
+
+public class OrderCreatedEventHandler(ILogger<OrderCreatedEventHandler> logger)
+    : INotificationHandler<OrderCreatedEvent>
+{
+    public Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Domain Event handled: {DomainEvent}", domainEvent.GetType().Name);
+        return Task.CompletedTask;
+    }
+}
